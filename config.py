870b50907031198b972ae8123c8b41cb859598a6
Amber Hayat <99331167+DHAmber@users.noreply.github.com>
2026-03-03 15:24:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 test_root_folder = "D:\\Amber\\Multimodel Root\\CancelableTemplate_girls\\output_dataset\\test"
-threshold=0.7
+threshold=0.6
 tz=64
 number_of_user=100
-template_size=512
+template_size=1024
 target_size = tz * tz
